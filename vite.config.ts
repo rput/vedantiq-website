bb6a548e63d6864a213a9de19ffcc8b36da083cf
Your Name <your.email@example.com>
2025-05-19 20:35:29 -0300

    fix: set Vite base to root for custom domain verdantiq.ca

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vedantiq-website/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'docs',
